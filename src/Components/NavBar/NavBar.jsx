f0128df7585aef17c3e825e0e6197fdc74eef165
Sakshi Srivastava <sakshi.rd369@gmail.com>
2026-06-22 21:43:17 +0530

    fix: navbar css file casing mismatch

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react'
-import './NavBar.css'
+import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
 import { StoreContext } from '../../Context/StoreContext';
